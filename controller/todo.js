7239e4c30bba67ff3377eebb85666b0a99201995
Brendan <brendan.ryan@pm.me>
2019-11-30 21:16:20 -0500

    began the process of using controllers and a db on todo app with basic set up modeled on yum app Arthur showed

---
+++ controller/todo.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const Todo = require('../models/Todo.js');
+
+router.get('/', (req, res) => {
+  Restaurant.find({}).then(restaurants => {
+    res.render('todo/Index', { restaurants });
+  });
+});
+
+// new
+router.get('/new', (req,res) => {
+    res.render('New');
+})
+// create
+router.post('/', (req,res) => {
+    Todo.create(req.body, (error, createdTodo)=>{
+        res.redirect('');
+    });
+});
+// read
+router.get('/:id', (req,res)=> {
+    Todo.findById(req.params.id, (err, foundTodo) => {
+        res.render('Show', {
+           todo:foundTodo
+        });
+    });
+});
+// delete
+router.delete('/:id', (req, res) => {
+    Todo.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect('/');
+    });
+});
+// update
+router.get('/:id/edit', (req, res) => {
+    Todo.findById(req.params.id, (err, foundTodo) => {
+        res.render('Edit.jsx', {
+            todo:foundTodo
+        });
+    })
+})
+// put
+router.put('/:id', (req, res) => {
+    Todo.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
+        // res.send(updatedModel);
+        res.redirect('/');
+    });
+})
+
+
+
+
+router.listen(4545, ()=>{
+    console.log('listening');
+});
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require('./db/connection.js');
 const Todo = require('./models/todo.js');
 const todoSeed = require('./models/seed.js')
 const methodOverride = require('method-override');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/basiccrud'
+// const Port = process.env.PORT || 4545
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/basiccrud'
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 // connect to mongoose
-mongoose.connect(MONGODB_URI, { 
+mongoose.connect('mongodb://localhost:27017/basiccrud', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: true
@@ -25,56 +26,3 @@ mongoose.connection.once('open', () => {
 // })
 
 // index view
-app.get('/', (req, res) =>{
-    Todo.find({}, (error,allTodo) => {
-        res.render('Index', {
-            todo: allTodo
-        });
-    });
-});
-// new
-app.get('/new', (req,res) => {
-    res.render('New');
-})
-// create
-app.post('/', (req,res) => {
-    Todo.create(req.body, (error, createdTodo)=>{
-        res.redirect('');
-    });
-});
-// read
-app.get('/:id', (req,res)=> {
-    Todo.findById(req.params.id, (err, foundTodo) => {
-        res.render('Show', {
-           todo:foundTodo
-        });
-    });
-});
-// delete
-app.delete('/:id', (req, res) => {
-    Todo.findByIdAndRemove(req.params.id, (err, data) => {
-        res.redirect('/');
-    });
-});
-// update
-app.get('/:id/edit', (req, res) => {
-    Todo.findById(req.params.id, (err, foundTodo) => {
-        res.render('Edit.jsx', {
-            todo:foundTodo
-        });
-    })
-})
-// put
-app.put('/:id', (req, res) => {
-    Todo.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
-        // res.send(updatedModel);
-        res.redirect('/');
-    });
-})
-
-
-
-
-app.listen(4545, ()=>{
-    console.log('listening');
-});
